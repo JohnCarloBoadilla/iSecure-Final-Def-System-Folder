43e5638b94281b47256e644781497feff13c25a8
Raizel-0410 <diegonate6@gmail.com>
2025-11-04 13:25:59 +0800

    di kaylangan na files

--- add_constraint.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'php/database/db_connect.php';
-try {
-    $pdo->exec('ALTER TABLE vehicles ADD UNIQUE KEY unique_visitation_id (visitation_id)');
-    echo 'Unique constraint added successfully';
-} catch (Exception $e) {
-    echo 'Error: ' . $e->getMessage();
-}
