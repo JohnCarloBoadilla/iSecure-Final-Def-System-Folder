ca3513218c35a48dfe03f023515a495b8dddbdc0
GabrielPecson <2022312029@pampangastateu.edu.ph>
2025-11-12 19:21:30 +0800

    Update favicon link path in home-page.php

--- php/routes/Pages/home-page.php
+++ php/routes/Pages/home-page.php
@@ -24,7 +24,7 @@ if (!$token) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="../../images/logo/5thFighterWing-logo.png">
+    <link rel="icon" type="image/png" href="5thFighterWing-logo.png">
     <title>5th Fighter Wing</title>
     <link href="../../src/output.css" rel="stylesheet" >
     <link rel="preconnect" href="https://fonts.googleapis.com">
