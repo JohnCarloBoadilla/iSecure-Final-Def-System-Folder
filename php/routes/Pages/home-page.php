fa322967714930650aa0cc95c6b8418b9e8129eb
GabrielPecson <2022312029@pampangastateu.edu.ph>
2025-11-12 19:15:03 +0800

    Update resource paths for logo and stylesheet

--- php/routes/Pages/home-page.php
+++ php/routes/Pages/home-page.php
@@ -24,9 +24,9 @@ if (!$token) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="http://isecured.online/images/logo/5thFighterWing-logo.png">
+    <link rel="icon" type="image/png" href="../../../images/logo/5thFighterWing-logo.png">
     <title>5th Fighter Wing</title>
-    <link href="http://isecured.online/src/output.css" rel="stylesheet" >
+    <link href="../../../src/output.css" rel="stylesheet" >
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
